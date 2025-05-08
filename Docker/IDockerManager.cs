0b33d9a86d9088a417e285ef6aea98988e75e063
Pete Field <peter.field@newday.co.uk>
2025-05-08 19:40:20 +0100

    add docker components

---
+++ Docker/IDockerManager.cs
@@ -0,0 +1,19 @@
+﻿using Docker.DotNet.Models;
+
+namespace pefi.servicemanager.Docker
+{
+    public interface IDockerManager
+    {
+        Task<ContainerListResponse?> CreateContainer(string packageUrl, string packageName, string? containerPortNumber, string? hostPortNumber);
+        Task CreateImage(string packageUrl);
+        Task DeleteImage(string packageUrl);
+        Task<ContainerListResponse?> GetContainerFromImageUrl(string packageUrl);
+        Task<ImagesListResponse?> GetImageFromImageUrl(string packageUrl);
+
+        Task StopContainer(string containerId);
+        Task StartContainer(string containerId);
+
+        Task RemoveContainer(string containerId);
+
+    }
+}
