99b74aaf5891faf41f5f92c2b427bd174214fa54
Pete Field <peter.field@newday.co.uk>
2025-05-08 15:30:18 +0100

    use id

--- ServiceDescription.cs
+++ ServiceDescription.cs
@@ -1,4 +1,25 @@
-﻿namespace pefi.servicemanager
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace pefi.servicemanager
 {
-    public record ServiceDescription(string ServiceName, string HostName, string? ContainerPortNumber, string? HostPortNumber);
+    public class ServiceDescription(
+        string ServiceName, string HostName, string? ContainerPortNumber, string? HostPortNumber)
+    {
+
+        [BsonId]
+        public ObjectId Id { get; set; } 
+
+        [BsonElement("ServiceName")]
+        public string ServiceName { get; set; } = ServiceName;
+
+        [BsonElement("HostName")]
+        public string HostName { get; set; } = HostName;
+
+        [BsonElement("ContainerPortNumber")]
+        public string? ContainerPortNumber { get; set; } = ContainerPortNumber;
+
+        [BsonElement("HostPortNumber")]
+        public string? HostPortNumber { get; set; } = HostPortNumber;
+    }
 }
