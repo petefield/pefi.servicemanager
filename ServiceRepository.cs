3359d7719d2d1e9a773615784710008a04dd777a
Pete Field <peter.field@newday.co.uk>
2025-05-08 15:23:18 +0100

    use db

--- ServiceRepository.cs
+++ ServiceRepository.cs
@@ -1,5 +1,7 @@
 ﻿using dnsimple;
+using MongoDB.Driver;
 using pefi.Rabbit;
+using System.Xml.Linq;
 
 namespace pefi.servicemanager
 {
@@ -10,16 +12,38 @@ namespace pefi.servicemanager
             Logger = logger;
         }
 
-        public List<ServiceDescription> Services { get; set; } = [];
+        public List<ServiceDescription> Services { 
+            get {
+                var connectionString = "mongodb://192.168.0.5:27017"; // Default Mongo URI
+                var client = new MongoClient(connectionString);
+                var database = client.GetDatabase("testdb");
+                var collection = database.GetCollection<ServiceDescription>("services");
+
+
+                var allPeople = collection.Find(_ => true).ToList();
+
+                return allPeople;
+            } 
+        }
         public ILogger<ServiceRepository> Logger { get; }
 
         public async Task< ServiceDescription>  Add(string Name, string? hostName, string? containerPortNumber, string? hostPortNumber)
         {
+            var connectionString = "mongodb://192.168.0.5:27017"; // Default Mongo URI
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("testdb");
+            var collection = database.GetCollection<ServiceDescription>("services");
+
+            var service = new ServiceDescription(Name, hostName, containerPortNumber, hostPortNumber);
+
+
+            collection.InsertOne(service);
+
+
             using var messageBroker = await pefi.Rabbit.MessageBroker.Create("192.168.0.5", "username", "password");
             using var topic = await messageBroker.CreateTopic("Events");
 
 
-            var service = new ServiceDescription(Name, hostName, containerPortNumber, hostPortNumber);
             Services.Add(service);
             await topic.Publish("events.service.created", service.ServiceName);
             Logger.LogWarning("message sent");
